be0ac5ba35d001fcadfdd4096f626e96912bfc74
José Davi <davimauricio527@gmail.com>
2026-06-02 00:21:33 -0300

    Add Javadoc comment for User class
    Added a Javadoc comment to describe the User class.

--- src/entities/User.java
+++ src/entities/User.java
@@ -1,5 +1,8 @@
 package src.entities;
 
+/**
+ * Entidade de Negócio que representa o modelo de dados puro de um Usuário do sistema.
+ */
 public class User {
     public int id;
     public String nome;
